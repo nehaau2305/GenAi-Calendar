272ec60ce2b2d22fe10ece411a2001f44716ace4
nehaau2305 <nehaau2305@gmail.com>
2026-07-15 15:30:16 -0400

    connect frontend & backend with example fetch call

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,21 @@
-import Image from "next/image";
+"use client";
+
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [message, setMessage] = useState("Loading...");
+  
+  useEffect(() => {
+    fetch("http://localhost:8000/")
+    .then((res) => res.json())
+    .then((data) => setMessage(data.message))
+    .catch(() => setMessage("Could not connect to backend"));
+  }, []);
+
   return (
     <main>
       <h1>GenAi Calendar</h1>
+      <p>{message}</p>
     </main>
   );
 }
